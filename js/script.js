be53ee9c07190906b287fffbb5fe71b8d41f1e86
erinreiss <reiss.aaron@gmail.com>
2014-11-10 12:01:58 -0500

    Chugging along: all markers return thier unique info upon click. Now how to get them to return latlng coords inthe corect format to get these babies to pan...

--- js/script.js
+++ js/script.js
@@ -1,59 +1,56 @@
-var map = L.map('map', {zoomControl: false}).setView([40.698824, -73.989312], 12);
+//Set Up The Map
+var map = L.map('map', {zoomControl: false})
+	.setView([40.698824, -73.989312], 13);
 
+//Set Up Basemap Tiles From Stamen
 L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
 	maxZoom: 17,
-	minZoom: 12
+	minZoom: 13
 }).addTo(map);
-
 L.control.zoom({position: 'bottomleft'}).addTo(map);
 
-//define House Icon
-
-	var houseIcon = L.icon({
-	    iconUrl:      'images/HomeIcon.png',
-	    iconSize:     [30, 30],
-	    iconAnchor:   [15, 15],
-	    popupAnchor:  [0, -15] 
+//Create My Custom Icon Variable
+var houseIcon = L.icon({
+	iconUrl:      'images/HomeIcon.png',
+	iconSize:     [30, 30],
+	iconAnchor:   [15, 15],
+	popupAnchor:  [0, -15] 
 })
-	
-//listeners for icons
 
-//house1.on('click', function(){
-//	map.panTo(new L.LatLng(40.681939, -73.979651), {animate: true, duration: 1.0});
-//		$('.addressFill').text("59 5th Ave.");
-//		$('.nabeFill').text("Park Slope");
-//		$('.timeFill').text("2 months");
-//		$('.infoFill').text("This was actually a closet. Somehow Lizzie and I shared this room for the two months after I first arrived in the city. Every morning we had to move our clothes from the floor to the bed to get out of the room.");
- //		$('#houseTitle').text("Park Slope Shoebox");
- 	//	$('#housePhoto').attr("src","images/house1pic.png");
+//Load External geoJSON (WTF is pointToLayer???)
+$.getJSON('./js/geoJSON.json',function(data){
+	//why is this a var?
+	var geojsonLayer = L.geoJson(data.features, {
+		onEachFeature: makeMarkers,
+		pointToLayer: function (feature, latlng) {
+			//can you define the custom icon var here?
+			return L.marker(latlng, {icon:houseIcon});
+		}
+	}).addTo(map);
+});
 
-//});
 
-//house2.on('click', function(e){
-//	map.panTo(new L.LatLng(40.674899,-73.963617), {animate: true, duration: 1.0});
-//	$('.addressFill').text("431 Sterling Pl.");
-//	$('.nabeFill').text("Prospect Heights");
-//	$('.timeFill').text("10 months");
-//	$('.infoFill').text("My first real home in NYC, I moved here with Lizzie after living in the City for only a few months. We shared this strange and ecclectic 3 bedroom apartment with three wonderful friends.");
-//	$('#houseTitle').text("A Strange New Cottage in Brooklyn");
-//    $('#housePhoto').attr("src","images/house2pic.png");
-//    console.log(e); 
-//});
+function makeMarkers(feature, layer) {
+	var thisFeature = feature.properties;
+		console.log(layer);
+		console.log(feature);
+		console.log(feature.properties.hTitle);
+	layer.on("click", function(e){
+		map.panTo(new L.LatLng(feature.coordinates.0,feature.coordinates.1), {animate: true, duration: 1.0});
+		$('#houseTitle').text(feature.properties.hTitle)
+		$('.addressFill').text(feature.properties.hAddress)
+		$('.nabeFill').text(feature.properties.hNabe)
+		$('.timeFill').text(feature.properties.hTime)
+		$('.infoFill').text(feature.properties.hInfo)
+		$('#housePhoto').attr("src",feature.properties.hPhoto)
+	});
 
-	// Strange New Cottage in Brooklyn was too long, how to wrap
+ };
 
-// house3.on('click', function(e){
-//	console.log(e);
-//	map.panTo(new L.LatLng(40.712934, -73.989794), {animate: true, duration: 1.0});
-//	$('.addressFill').text("27 Rutgers St.");
-//	$('.nabeFill').text("Chinatown");
-//	$('.timeFill').text("1 year");
-//	$('.infoFill').text("This is where I live now. I live on the 2nd floor of a Chinatown walk-up, above a Chinese food restaurant called \"Forever Taste.\" I love the area, I get to practice Chinese and I live two blocks from the East River.");
-//	$('#houseTitle').text("Chinatown Home-Stay");
-//    $('#housePhoto').attr("src","images/house3pic.png");
 
-//});
+
+
 
 //Defines what happens when marker is clicked
 
@@ -62,39 +59,6 @@ L.control.zoom({position: 'bottomleft'}).addTo(map);
 //	console.log(this)
 //	console.log("boo")
 //};
-
-function buttonSetup(feature, layer) {
-	var thisFeature = feature.properties;
-
-	console.log(layer);
-	console.log(feature.properties.hTitle);
-	layer.bindPopup(
-		feature.properties.hTitle
-		);
-//	"<div class ='"
-//	feature.properties.hTitle"'
-//    $('#houseTitle').text(feature.properties.hTitle)
-//	feature.properties.hTitle.getElementByID = ;
-	
-	layer.on("click", function(e){
-		$('#houseTitle').text(feature.properties.hTitle)
-	});
-
- };
-
-//This function adds custom markers - WTF is pointToLayer???
-
-function putMarker(feature, latlng) {
-    return L.marker(latlng, {icon:houseIcon});
-};
-
-$.getJSON('./js/geoJSON.json',function(data){
-	console.log(data);
-
-L.geoJson(data.features, {
-	onEachFeature: buttonSetup, pointToLayer: putMarker
-	}).addTo(map);
-});
 
 
 //That hail mary that Chris found to first get geoJSON to use my custom markers
